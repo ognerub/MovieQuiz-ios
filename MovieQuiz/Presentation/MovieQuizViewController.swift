aae4dcb5eba3c4b4995baf757117784f0df64e91
Alexander Ulianovskiy <a.ulianovskiy@yandex.ru>
2023-06-21 00:34:30 +0300

    корректировка мелочей в MVC

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,25 +1,14 @@
 import UIKit
 
-// добавляем в объявление класса реализацию протокола делегата
 final class MovieQuizViewController: UIViewController {
     
     // MARK: - Properties
-    // переменные из экрана
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var counterLabel: UILabel!
     @IBOutlet weak var yesButton: UIButton!
     @IBOutlet weak var noButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    /// переменная со счётчиком правильных ответов, начальное значение закономерно 0
-    //private var correctAnswers = 0
-    /// переменная фабрики вопросов подписанная под протокол
-    //private var questionFactory: QuestionFactoryProtocol?
-    /// переменная алерт сообщения подписанная под протокол
-    //private var alertPresenter: AlertPresenterProtocol?
-    ///  переменная сервиса статистики
-    //private var statisticService: StatisticService?
-    /// переменная MVP (презентера) созданная после рефакторинга
     private var presenter = MovieQuizPresenter()
     
     // MARK: - Lifecycle
@@ -27,8 +16,7 @@ final class MovieQuizViewController: UIViewController {
         super.viewDidLoad()
         /// добавляем чтобы заработала связь с MVP
         presenter.viewController = self
-        presenter.viewDidLoad()
-        
+        presenter.viewDidLoad()      
     }
     
     func didReceiveNextQuestion(_ question: QuizQuestion?) {
@@ -49,22 +37,19 @@ final class MovieQuizViewController: UIViewController {
     func showAnswerResult() {
         presenter.showAnswerResult()
     }
-    
+    /// метод определяющий что отображать, следующий вопрос или результат игры
     func showNextQuestionOrResults() {
         presenter.showNextQuestionOrResults()
     }
-    
     /// метод, отображающий алерт с ошибкой загрузки
     func showNetworkError(message: String) {
         presenter.showNetworkError(message: message)
     }
     
     // MARK: - Actions
-    
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         presenter.yesButtonClicked()
     }
-    
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         presenter.noButtonClicked()
     }
